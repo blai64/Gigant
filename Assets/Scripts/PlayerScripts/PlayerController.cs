f7bf8bf6c8a753a1d26911bb560dba07480175e2
Brian Lai <bhlai@andrew.cmu.edu>
2017-04-03 12:12:43 -0400

    added vertical basic movement

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -7,8 +7,16 @@ public class PlayerController : MonoBehaviour {
 	[HideInInspector] public bool isLeft = false;
 
 	public float maxSpeed = 10f; 
+	public float jumpForce = 10f; 
 
-	private int direction;
+	//lateral movement
+	private int direction; // [-1,0,1], for determining direction of velocity
+
+	//vertical movement
+	private int maxJumps = 2; 
+	private int remainingJumps; // Remaining number of jumps
+	private bool doJump; // try to start jump on current frame
+
 
 	public Transform groundCheck;
 	private Rigidbody2D rb2d;
@@ -16,15 +24,24 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		isGrounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+		remainingJumps = (isGrounded) ? maxJumps : remainingJumps;
+
 		InputManager ();
 
+		//update lateral movement
 		rb2d.velocity = new Vector2 (direction * maxSpeed, rb2d.velocity.y);
+
+		//update vertical movememnt
+		if (doJump){
+			remainingJumps--;
+			rb2d.velocity =  new Vector2 (rb2d.velocity.x, jumpForce);
+			doJump = false; 
+		}
 	}
 
 	void InputManager(){
@@ -37,6 +54,11 @@ public class PlayerController : MonoBehaviour {
 		else if (Input.GetKey (KeyCode.RightArrow)) {
 			isLeft = false; 
 			direction = 1; 
+		}
+
+		//Vertical Movement
+		if (remainingJumps > 0 && Input.GetKeyDown(KeyCode.UpArrow)){
+			doJump = true;
 		}
 	}
 }
