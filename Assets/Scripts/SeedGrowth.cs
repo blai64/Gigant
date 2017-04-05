6058dc194d77b77b2e457959d45ab4866f772aed
Alexander Messam <alexmessam32@gmail.com>
2017-04-05 11:27:17 -0400

    i got nothin

---
+++ Assets/Scripts/SeedGrowth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedGrowth : MonoBehaviour {
+	bool grown;
+	// Use this for initialization
+	void Start () {
+		grown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.localScale.x < .5f) {
+			transform.localScale += new Vector3 (.05f, .1f,0);
+		} else
+			grown = true;
+	}
+}
