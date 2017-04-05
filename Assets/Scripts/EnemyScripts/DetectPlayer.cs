5e351337ea02596d65b8183a78af08ffe7c3a61f
Brian Lai <bhlai@andrew.cmu.edu>
2017-04-04 20:17:50 -0400

    started working on enemy detection/activation scripts

---
+++ Assets/Scripts/EnemyScripts/DetectPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour {
+
+	void onTriggerEnter2D (Collider2D col){
+		if (col.CompareTag("Player")){
+			Activate ();
+		}
+	}
+
+	//Use this function to dictate the "awakening" of enemies
+	void Activate(){
+	
+	}
+
+}
