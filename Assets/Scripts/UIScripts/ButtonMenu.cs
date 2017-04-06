39758e4812119f0789667f107b3fcaad94bcec9f
Michelle Ma <michell3@andrew.cmu.edu>
2017-04-06 11:19:22 -0400

    allll the assets

---
+++ Assets/Scripts/UIScripts/ButtonMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour {
+
+	private Button btn;
+	private string btnName;
+	private PauseGame pg;
+
+	void Awake() {
+		
+		btn = GetComponent<Button> ();
+		btnName = btn.GetComponentInChildren<Text>().text;
+
+		GameObject UI = GameObject.Find ("UI Controller");
+		if (UI != null) {
+			pg = UI.GetComponent<PauseGame> ();
+			pg.Pause ();
+		}
+
+		switch (btnName) {
+
+		case "Play":
+			btn.onClick.AddListener (delegate{ChangeScene("Michelle Scene");});
+			break;
+
+		case "Main Menu":
+			btn.onClick.AddListener (delegate{ChangeScene("Splash");});
+			break;
+
+		case "Instructions":
+			btn.onClick.AddListener (delegate{ChangeScene("Instructions");});
+			break;
+
+		case "Credits":
+			btn.onClick.AddListener (delegate{ChangeScene("Credits");});
+			break;
+
+		case "Quit":
+			btn.onClick.AddListener (Quit);
+			break;
+
+		case "Resume":
+			btn.onClick.AddListener (Resume);
+			break;
+
+		case "Mute":
+			btn.onClick.AddListener (Mute);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	void Resume() {
+		pg.Pause ();
+	}
+
+	void Mute() {
+		// TODO
+	}
+
+	void ChangeScene(string sceneName) {
+		if (pg != null) Resume ();
+		SceneManager.LoadScene (sceneName);
+	}
+
+	void Quit() {
+		if (pg != null) Resume ();
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/UIScripts/PauseGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public Transform canvas;
+	private bool paused = false;
+
+	void Start() {
+		canvas.gameObject.SetActive (false);
+		paused = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Debug.Log ("pause");
+			Pause();
+		}
+	}
+
+	public void Pause() {
+
+		if (!paused) {
+			canvas.gameObject.SetActive (true);
+			paused = true;
+			Time.timeScale = 0;
+		} else {
+			canvas.gameObject.SetActive (false);
+			paused = false;
+			Time.timeScale = 1;
+
+		}
+	}
+
+	public bool isPaused() {
+		return paused;
+	}
+}
