28d9e9df5a6ddad2698fcb249489da0b10ab86b4
楼早 <Mabel@louzaos-MBP.wv.cc.cmu.edu>
2017-04-10 11:58:17 -0400

    healthbar

--- Assets/Scripts/SceneScripts/Health.cs
+++ Assets/Scripts/SceneScripts/Health.cs
@@ -5,24 +5,26 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
-	public GameObject heart1;
-	public GameObject heart2;
-	public GameObject heart3;
-
-	private int hp;
-
 	public static Health instance;
+
+	public GameObject life1;
+	public GameObject life2;
+	public GameObject life3;
+
+	public int hp;
 	private bool reset; //use boolean variable reset to check if health is reset.
 
 	// Use this for initialization
 	void Start () {
-		reset = true;
-		if (reset) {
-			hp = 3;
-		} else{
+		if (SceneManager.GetActiveScene ().name == "Zao_tutorial")
+			reset = true;
+		//if (reset) {
+		//	hp = 3;
+		//} 
+		//else{
 			hp = PlayerPrefs.GetInt ("health");
-		}
-}
+		//}
+	}
 
 	void Awake(){
 		if (instance == null)
@@ -31,23 +33,24 @@ public class Health : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		
 		//Debug.Log (timer);
 		if (hp == 3) {
-			heart1.SetActive (true);
-			heart2.SetActive (true);
-			heart3.SetActive (true);
+			life1.SetActive (true);
+			life2.SetActive (true);
+			life3.SetActive (true);
 		} else if (hp == 2) {
-			heart1.SetActive (true);
-			heart2.SetActive (true);
-			heart3.SetActive (false);
+			life1.SetActive (true);
+			life2.SetActive (true);
+			life3.SetActive (false);
 		} else if (hp == 1) {
-			heart1.SetActive (true);
-			heart2.SetActive (false);
-			heart3.SetActive (false);
+			life1.SetActive (true);
+			life2.SetActive (false);
+			life3.SetActive (false);
 		} else if (hp == 0) {
-			heart1.SetActive (false);
-			heart2.SetActive (false);
-			heart3.SetActive (false);
+			life1.SetActive (false);
+			life2.SetActive (false);
+			life3.SetActive (false);
 		}
 
 
