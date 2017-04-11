8192e8a4e42057c4962ebbde419862f5f6c4bfcf
Brian Lai <bhlai@andrew.cmu.edu>
2017-04-11 17:49:43 -0400

    added basic cutscenes

--- Assets/Scripts/CameraScripts/CameraManager.cs
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
+	public static CameraManager instance;
 
-	// Use this for initialization
 
 	private Camera cam;
 
@@ -21,6 +21,13 @@ public class CameraManager : MonoBehaviour {
 	public GameObject BottomBlack;
 
 
+	void Awake(){
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (this);
+	}
+
 	void Start () {
 		cam = GetComponent<Camera> ();
 		Debug.Log (cam.orthographicSize); 
@@ -30,20 +37,24 @@ public class CameraManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
 		if (Input.GetKeyDown (KeyCode.Z) && myLock && !isActive) {
 			myLock = false;
-			StartCoroutine (Zoom (5.0f));
+			StartCoroutine (Zoom (true));
 			StartCoroutine (MoveCinematic (true));
 		} else if (Input.GetKeyDown (KeyCode.X) && myLock && isActive) {
 			myLock = false;
-			StartCoroutine (Zoom (10.0f));
+			StartCoroutine (Zoom (false));
 			StartCoroutine (MoveCinematic (false));
 		}
+*/
 	}
 
-	IEnumerator Zoom(float newCameraSize){
+	public IEnumerator Zoom(bool zoomIn){
 		float t = 0f;
 		float curCameraSize = cam.orthographicSize;
+
+		float newCameraSize = (zoomIn) ? 5.0f : 10.0f;
 
 		while (t < 1.0) {
 			t += Time.deltaTime / cinematicStartDuration;
@@ -54,7 +65,7 @@ public class CameraManager : MonoBehaviour {
 		//StartCoroutine (StartCinematic ());
 	}
 
-	IEnumerator MoveCinematic(bool starting){
+	public IEnumerator MoveCinematic(bool starting){
 		float t = 0f;
 
 		Vector3 oldTopPosition = TopBlack.transform.localPosition;
