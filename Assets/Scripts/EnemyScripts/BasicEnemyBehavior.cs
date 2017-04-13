dc22eb5dceaad93a01e56c04e52f1ead910a98ab
Brian Lai <bhlai@andrew.cmu.edu>
2017-04-13 16:22:01 -0400

    added basic enemy attack logic

--- Assets/Scripts/EnemyScripts/BasicEnemyBehavior.cs
+++ Assets/Scripts/EnemyScripts/BasicEnemyBehavior.cs
@@ -3,6 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicEnemyBehavior : BaseEnemyBehavior {
+	private int attackCounter;
+	private int attackThreshold = 150;
+	private float attackDistance = 6f;
+
+	public GameObject EnemyHitbox;
+
+	override protected void Update(){
+		base.Update ();
+		if (base.isActive && !base.isDead && !base.isAttacking) {
+			attackCounter += 1;
+			Debug.Log (attackCounter);
+
+			if (attackCounter > attackThreshold) {
+				StartAttack ();
+				attackCounter = 0;
+			}
+
+		}
+
+
+	}
 
 	//TODO: deprecated
 	override protected IEnumerator Activate(){
@@ -18,8 +39,12 @@ public class BasicEnemyBehavior : BaseEnemyBehavior {
 	virtual protected void StartAttack(){
 		base.isAttacking = true;
 		//start animation
+
+		//temporary coroutine to call the attack;
+		DoAttack();
 	}
 
+	//ANIMATION EVENT should call this when 	the animation is over
 	virtual protected void EndAttack(){
 		//do any clean up here.
 		base.isAttacking = false;
@@ -28,6 +53,25 @@ public class BasicEnemyBehavior : BaseEnemyBehavior {
 	//ANIMATION EVENT should call this to do damage
 	public void DoAttack(){
 		//attack logic, e.g. creating hitbox in front of enemy, throwing object, etc
+		StartCoroutine(BasicAttack());
+	}
+
+	IEnumerator BasicAttack(){
+		float t = 0f; 
+		GameObject temp = Instantiate (EnemyHitbox, transform.position, transform.rotation);
+
+		Vector3 oldPos = temp.transform.position;
+		Vector3 newPos = oldPos + (base.direction * attackDistance * Vector3.right);
+
+		while (t < 1.0) {
+			t += Time.deltaTime / 0.5f;
+
+			temp.transform.position = Vector3.Lerp (oldPos, newPos, t);
+			yield return 0;
+		}
+
+		Destroy (temp);
+		EndAttack ();
 	}
 
 
