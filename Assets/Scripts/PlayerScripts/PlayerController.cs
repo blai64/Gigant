d623c50e7acc654e7d0986d0d9ed572a958e769c
Alexander Messam <alexmessam32@gmail.com>
2017-04-27 15:44:44 -0400

    enemy hitbox fix

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -365,7 +365,7 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
-		if (col.gameObject.CompareTag ("Enemy")) {
+		if (col.gameObject.CompareTag ("Damage")) {
 			Health.instance.hp--;
 			Knocked ();
 			hurting = true;
