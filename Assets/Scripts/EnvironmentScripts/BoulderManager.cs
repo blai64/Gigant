08bc74cb6fc18545251b6f3fc22e7a337fb9de27
Michelle Ma <michell3@andrew.cmu.edu>
2017-04-30 17:25:27 -0400

    TILESSSS

--- Assets/Scripts/EnvironmentScripts/BoulderManager.cs
+++ Assets/Scripts/EnvironmentScripts/BoulderManager.cs
@@ -9,6 +9,11 @@ public class BoulderManager : MonoBehaviour {
 	public GameObject exclamationPointPrefab;
 	public Transform spawnTrans;
 	public Transform disappearTrans;
+	public float rotateSpeed = 500f;
+	public float scaleMin = 0.8f;
+	public float scaleMax = 1.0f;
+	private float newRotate;
+
 	private Vector3 spawnPos;
 	private Rigidbody2D rb2d;
 
@@ -16,26 +21,28 @@ public class BoulderManager : MonoBehaviour {
 	public bool reset;
 	public float range;
 
-
-
-
-	// Use this for initialization
 	void Start () {
 		spawnPos = spawnTrans.position;
 		boulderPrefab.SetActive (false);
 		startFalling = false;
 		reset = true;
 		rb2d = boulderPrefab.GetComponent<Rigidbody2D> ();
+		newRotate = Random.Range (-rotateSpeed, rotateSpeed);
 	}
 
-
-
-	void Update(){
+	void Update() {
+		
 		boulderPrefab.SetActive (startFalling);
+
 		if (reset) {
-			boulderPrefab.transform.position = new Vector3 (spawnPos.x + range * Random.value, spawnPos.y, spawnPos.z);
+			boulderPrefab.transform.position = new Vector3 (spawnPos.x + range * Random.Range(-1f, 1f),
+															spawnPos.y, spawnPos.z);
+			float newScale = Random.Range (scaleMin, scaleMax);
+			boulderPrefab.transform.localScale = new Vector3 (newScale, newScale, newScale);
+			newRotate = Random.Range (-rotateSpeed, rotateSpeed);
 			rb2d.velocity = new Vector2 (0.0f, 0.0f);
 		}
+
 		if (startFalling) {
 			if (reset) {
 				GameObject warning = Instantiate (exclamationPointPrefab);
@@ -55,13 +62,11 @@ public class BoulderManager : MonoBehaviour {
 	}
 
 	void Rotate(GameObject obj){
-		obj.transform.Rotate(Vector3.back * Time.deltaTime * 1000);
+		obj.transform.Rotate(Vector3.back * Time.deltaTime * newRotate);
 	}
 
-
 	void OnTriggerEnter2D (Collider2D col){
-		if (col.CompareTag("Player")){
-			//StartCoroutine (StartFalling ());
+		if (col.CompareTag("Player")) {
 			startFalling = true;
 		}
 	}
