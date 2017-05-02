8fdf407d18d76c4cf519cddf3c2e780c9558ff72
Brian Lai <bhlai@andrew.cmu.edu>
2017-05-02 17:20:06 -0400

    added debris indicators, dust clounds on impact. Still need to restrict appearance zones:

---
+++ Assets/Scripts/EnvironmentScripts/DebrisScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisScript : MonoBehaviour {
+
+	public GameObject dustParticleSpawner;
+
+	void OnTriggerEnter2D (Collider2D col){
+		
+		if (col.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+			GameObject ps = Instantiate (dustParticleSpawner);
+			ps.transform.position = transform.position;
+			/*
+			ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams ();
+
+			emitParams.position = transform.position;
+			dustParticleSpawner.GetComponent<ParticleSystem> ().Emit (emitParams,2);
+			//dustParticleSpawner.GetComponent<ParticleSystem> ().Play ();*/
+		}
+
+	}
+}
